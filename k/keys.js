ae6a4909b028a28241b63c46065dc8b4f19f90ff
Nicholas N <nnlaor@gmail.com>
2017-04-03 13:27:03 -0700

    testing

--- k/keys.js
+++ k/keys.js
@@ -2,8 +2,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'HXRSmIhu9PDXn7rWry5LGZS5P',
+  consumer_secret: 'aFAPIIMLfoARw9a3Y4EFX5uVM9DXHPl8oONxTodI2Ex2okOImw',
+  access_token_key: '1013602849-bsRxdLqqhrAXD63hSrsk0Xdi2q5f6q5GOb7crC5',
+  access_token_secret: 'bYFW6vN7h9uKlaGXexUbGhMp45D2Iyx81uNlXfes7xb3K',
 }
