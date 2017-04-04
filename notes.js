30b2d9d3c66fb18e71796b05e11f6519cf27cf2b
Nicholas N <nnlaor@gmail.com>
2017-04-04 13:28:15 -0700

    making magic

---
+++ notes.js
@@ -0,0 +1,56 @@
+
+// Require
+const https = require("https"); 
+
+// Problem: We need a simple way to look at a user's badge count and JavaScript points
+// Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
+//print message to console
+
+
+function printMessage(username, badgeCount, points){
+  const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
+  console.log(message);
+}
+
+function getProfile(username){
+
+
+const request = https.get(`https://teamtreehouse.com/${username}.json`, response =>{
+                          // returns the reponse object
+//                          console.dir(response);
+//                          console.log(response.statusCode);
+
+                          let body = "";
+                          response.on('data', data => {
+                            // this will actualy return a buffer, data type
+                            body += data.toString();
+//                          console.log('data:', data);
+//                            console.log('data:', data.toString());
+                          });
+
+                            response.on('end', () => {
+                              const profile = JSON.parse(body);
+                              // this willl show you an object .dir
+//                            console.dir(profile);
+//                            console.log(body);
+//                            console.log(typeof body);
+                              printMessage(username, profile.badges.length, profile.points.JavaScript);
+                            
+         
+                            // Print the data
+                          });
+                          
+                          });
+}
+console.log(process.argv);
+//console.dir(process);
+const users = ["chalkers", "alenaholligan", "nnoochlaor", "davemcfarland"]
+
+users.forEach(getProfile);
+
+
+//getProfile("nnoochlaor");
+// Connect to the API URL (`https://teamtreehouse.com/${username}.json`)
+// Read the data
+// Parse the data
+// Print the data
