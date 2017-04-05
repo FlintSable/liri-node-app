32c0b7fee1dfb3570c6c74e12a981be338cec763
Nicholas N <nnlaor@gmail.com>
2017-04-05 07:45:48 -0700

    adding .on resepnse and .end

--- k/keys.js
+++ k/keys.js
@@ -6,4 +6,4 @@ module.exports.twitterKeys = {
   consumer_secret: 'aFAPIIMLfoARw9a3Y4EFX5uVM9DXHPl8oONxTodI2Ex2okOImw',
   access_token_key: '1013602849-bsRxdLqqhrAXD63hSrsk0Xdi2q5f6q5GOb7crC5',
   access_token_secret: 'bYFW6vN7h9uKlaGXexUbGhMp45D2Iyx81uNlXfes7xb3K',
-}
+};
