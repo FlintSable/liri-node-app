19ba010bc2df658007a1d14a2eaa7cfd68dbd872
Nicholas Noochla-or <nnlaor@gmail.com>
2017-04-06 16:24:29 -0700

    Delete keys.js

--- k/keys.js
+++
@@ -1,9 +0,0 @@
-
-console.log('this is loaded');
-
-module.exports.twitterKeys = {
-  consumer_key: 'HXRSmIhu9PDXn7rWry5LGZS5P',
-  consumer_secret: 'aFAPIIMLfoARw9a3Y4EFX5uVM9DXHPl8oONxTodI2Ex2okOImw',
-  access_token_key: '1013602849-bsRxdLqqhrAXD63hSrsk0Xdi2q5f6q5GOb7crC5',
-  access_token_secret: 'bYFW6vN7h9uKlaGXexUbGhMp45D2Iyx81uNlXfes7xb3K',
-};
