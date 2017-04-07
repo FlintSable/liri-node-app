21ee52bda79185fc618c59a62cc2ff6293922df9
Nicholas N <nnlaor@gmail.com>
2017-04-06 23:48:14 -0700

    spotify response added, very dense

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# liri-node-app
-node command line application 
+# HW - {Liri}
+-------------
+
+## Requirements
+#### Add a simple description of what the HW requirements were
+
+- node liri.js help
+- node liri.js my-tweets
+- node liri.js spotify-this-song songName
+- node liri.js movie-this movieName
+- node liri.js do-what-it-says 
+
+
+
+
+## Technologies Used
+- npm
+- AJAX for API GET requests
+
+## Code Explaination
+- Here, here
+- Here, here
+- Here, here
+- Here, here
+
+
+-------------
+
+##Here is an example of what a Readme could look like:
+
+### AJAX Request to Giphy (Example)
+I wrote this applcation. 
+
+```
+code goes here
+```
--- liri-spotify-this-song.js
+++ liri-spotify-this-song.js
@@ -1,8 +1,43 @@
 const spotify = require('spotify');
-
+console.log(__filename);
 // var program = require('..');
 
-console.log(__filename);
-// console.log(arguments.callee.toString());
 
 console.log(process.argv.slice(2));
+
+
+function songSearch() {
+    spotify.search({ type: 'track', query: 'black sands' }, function(err, data) {
+
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+        } else if (!err) {
+        	console.log(data.tracks.items);
+            // var localData = data.tracks.items[0];
+            // console.log(localData.album.artists[0].name);
+
+            // var artist = data.album.artist
+            // var songnName = 
+            // var preview = preview_url
+            // var album = 
+        }
+
+        // Do something with 'data' 
+    });
+
+}
+
+
+function CustomView(artist, songName, preview, album){
+
+}
+
+songSearch();
+// lookup: function({ type: 'artist OR album OR track', id: 'Spotify ID Hash' }, hollaback)
+// search: function({ type: 'artist OR album OR track', query: 'My search query' }, hollaback)
+// get: function(query, hollaback) -- See http://developer.spotify.com/en/metadata-api/overview/
+// Artist(s)
+// The song's name
+// A preview link of the song from Spotify
+// The album that the song is from
